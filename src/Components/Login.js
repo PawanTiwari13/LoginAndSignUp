42c72a55fb2905c1095fc81310886b3357f47a64
PawanTiwari13 <pt349031@gmail.com>
2023-09-05 10:44:22 +0530

    Completed Login Screen

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,60 +1,68 @@
-import React, {useState} from 'react';
-import './Login.css'
+import React, { useState } from "react";
+import "./Login.css";
 function Login() {
-    const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
 
-    const handleLogin = () => {
-        console.log('Username:', username);
-    };
+  const handleLogin = () => {
+    console.log("Username:", username);
+  };
 
-    return (
-        <div>
-            <div className="login-container">
-                <div className='atlassian-view'>
-<img  className='image' src='../../src/Images/atlassian.png'/>
-                    <text className='atlassian'>
-                        ATLASSIAN
-                    </text>
-
-                                    </div>
-
-                <h2>Login to continue</h2>
-                <div>
-                    <input
-                        type="text"
-                        placeholder=" Enter Your Email"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                </div>
-                <div  >
-                    <button className=' button' onClick={handleLogin}>Continue</button>
-                </div>
-                <div className='or'>
-                    <text>
-                        OR
-                    </text>
-                </div>
-                <div >
-                    <button className='google'>
-                        Continue with Google
-                    </button>
-                </div>
-
-                <div>
-                    <button>
-                        Continue with Microsoft
-                    </button>
-                </div>
-                <div>
-                    <button>
-                        Continue with Apple
-                    </button>
-                </div>
-
-            </div>
+  return (
+    <div>
+      <div className="login-container">
+        <div className="atlassian-view">
+          <img className="image" src="../../src/Images/atlassian.png" />
+          <text className="atlassian">ATLASSIAN</text>
         </div>
-    );
+
+        <h4 className="loginToContinue">Login to continue</h4>
+        <div>
+          <input
+            type="text"
+            placeholder=" Enter Your Email"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <button className=" button" onClick={handleLogin}>
+            Continue
+          </button>
+        </div>
+        <div className="or">
+          <text>Or continue with:</text>
+        </div>
+        <div className="google">
+          <img className="icon" src="../../src/Images/atlassian.png" />
+
+          <a className="signInOption">Google</a>
+        </div>
+
+        <div className="google">
+          <img className="icon" src="../../src/Images/atlassian.png" />
+          <text className="signInOption">Microsoft</text>
+        </div>
+        <div className="google">
+          <img className="icon" src="../../src/Images/atlassian.png" />
+
+          <a className="signInOption">Apple</a>
+        </div>
+
+        <div className="otherOption">
+          <text className="CantLoginIn">can't log in?</text>
+          <text className="CantLoginIn">Create an account</text>
+        </div>
+        <div className="separator"> </div>
+        <div className="atlassian-view-two">
+        <img className="imageOne" src="../../src/Images/atlassian.png" />
+          <text className="atlassianOne">ATLASSIAN</text>
+                </div>
+                <div>
+                    <text className="text">One account for Jira, Confluence, Trello and <text className="more">more</text></text>
+                </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
